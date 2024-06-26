6825b3cdda580a4e3dd095d2acef167313c8e0b7
Manoj R gowda <160026926+Manojrgowdaa@users.noreply.github.com>
2024-06-26 15:10:47 +0530

    Add files via upload

---
+++ interview programs/AddThree.java
@@ -0,0 +1,8 @@
+public class AddThree {
+   public static int add(int a, int b,int c) {
+	   return a+b+c;
+   }
+   public static void main(String[] args) {
+	System.out.println("Sum :"+add(2,3,4));
+}
+}
---
+++ interview programs/reversemultiple.java
@@ -0,0 +1,11 @@
+public class reversemultiple {
+   public static void main(String[] args) {
+	int num=25;
+	while(num>=1) {
+		if(num%5==0) {
+			System.out.println(num);
+		}
+		num--;
+	}
+}
+}
