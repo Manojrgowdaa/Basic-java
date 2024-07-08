58975915e0757f704f3c1f06f3f6bf341075fe5f
Manoj R gowda <160026926+Manojrgowdaa@users.noreply.github.com>
2024-07-08 12:02:14 +0530

    Delete interview programs directory

--- interview programs/AddThree.java
+++
@@ -1,8 +0,0 @@
-public class AddThree {
-   public static int add(int a, int b,int c) {
-	   return a+b+c;
-   }
-   public static void main(String[] args) {
-	System.out.println("Sum :"+add(2,3,4));
-}
-}
--- interview programs/Mobile.java
+++
@@ -1,9 +0,0 @@
-class Mobile{
-    String brand="Oppo";
-    double cost=30000;
-    public static void main(String[] args) {
-        Mobile m1=new Mobile();
-        System.out.println("Brand:" +m1.brand);
-        System.out.println("Brand:" +m1.cost);
-    }
-}
