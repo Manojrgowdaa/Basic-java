4eab0fd13ad98e4e61e8281e55933cfc8088bd03
Manoj R gowda <160026926+Manojrgowdaa@users.noreply.github.com>
2024-07-08 12:05:14 +0530

    Add files via upload

---
+++ conditionalstatements/compare.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class compare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+
+        // Prompt the user to enter the first number
+        System.out.print("Enter the first number: ");
+        a = sc.nextInt();
+
+        // Prompt the user to enter the second number
+        System.out.print("Enter the second number: ");
+         b = sc.nextInt();
+
+        // Compare the two numbers and print the result
+        if (a > b) {
+            System.out.println("a is greater than b");
+        } else if (a == b) {
+            System.out.println("a is equal to b");
+        } else {
+            System.out.println("a is less than b");
+        }
+
+        // Close the scanner
+        sc.close();
+    }
+}
